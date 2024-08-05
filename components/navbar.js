7ee22345f31276c82f4d54c1e49e8a7f6d3a4b33
Maryam Badghisi <zcbtadg@ucl.ac.uk>
2024-08-05 15:12:34 +0100

    changes

--- components/navbar.js
+++ components/navbar.js
@@ -2,13 +2,13 @@ export function Navbar() {
     return `
         <nav class="nav-menu">
             <div class="container">
-                <a href="routes/home" class="brand" onclick="navigateTo('home')">Badghisi</a>
+                <a href="#" class="brand" onclick="navigateTo('home')">Badghisi</a>
                 <div class="menu">
                     <a href="#" onclick="navigateTo('skills')">Skills</a>
                     <a href="#" onclick="navigateTo('projects')">Projects</a>
                     <a href="#" onclick="navigateTo('experience')">Experience</a>
                     <a href="#" onclick="navigateTo('education')">Education</a>
-                    <a href="#" onclick="navigateTo('resume')">Resumé</a>
+                    <a href="#" onclick="navigateTo('resume')">Resume</a>
                 </div>
                 <div class="icons">
                     <button id="theme-toggle"><img src="assets/icons/sun.svg" alt="Toggle Theme"></button>
